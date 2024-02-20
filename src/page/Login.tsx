2aa2c465693fdfa37f3d357d3a69024a69be3ece
Tech5818 <pqow5818@gmail.com>
2024-02-20 15:48:49 +0900

    :sparkles: react-hook-form을 이용한 form 핸들링

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -1,8 +1,22 @@
-import { Box, Input, Typography } from "@mui/material";
-import { useState } from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import { useForm } from "react-hook-form";
+
+interface IFormData {
+  name: string;
+}
 
 export const Login = () => {
-  const [userId, setUserId] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<IFormData>();
+
+  const handleFormSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
   return (
     <>
       <Box
@@ -12,24 +26,42 @@ export const Login = () => {
         justifyContent="center"
         alignItems="center"
       >
-        <Box
+        <Stack
+          component="form"
           boxShadow="0 0 10px -5px rgba(0, 0, 0, 0.4)"
           width="500px"
-          height="400px"
-          display="flex"
-          alignContent="center"
           padding="30px"
-          flexDirection="column"
+          direction="column"
+          alignItems="center"
+          spacing={2}
+          onSubmit={handleFormSubmit}
         >
           <Typography fontSize="36px" fontWeight="bold">
             정보 입력
           </Typography>
-          <Input
-            value={userId}
-            onChange={(e) => setUserId(e.target.value)}
-            placeholder="ID를 입력해주세요"
+          <TextField
+            label="이름"
+            placeholder="이름을 입력해 주세요"
+            {...register("name", {
+              minLength: {
+                message: "이름의 최소 길이는 3자 이상입니다",
+                value: 3,
+              },
+            })}
+            fullWidth
+            helperText={errors.name?.message}
+            error={!!errors.name?.message}
           />
-        </Box>
+          <Button
+            type="submit"
+            fullWidth
+            variant="outlined"
+            disabled={!watch("name")}
+            size="large"
+          >
+            보내기
+          </Button>
+        </Stack>
       </Box>
     </>
   );
